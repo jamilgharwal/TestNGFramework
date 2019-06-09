84e21393da0ff3cfd2f2e1acf0f751386b5a60e5
jamilgharwal <jamilgharwal195@gmail.com>
2019-06-09 11:49:16 -0400

    GitPractice Demo  class added to project

---
+++ src/main/java/FrameWork/TestNG/GitPracticeDemo.java
@@ -0,0 +1,11 @@
+package FrameWork.TestNG;
+
+public class GitPracticeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is repositry practice");
+	}
+
+}
